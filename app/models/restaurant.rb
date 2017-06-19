eebcf96a9f347bbb595468d8c3bd5126efb889de
Emily Boucher <emi.boucher@gmail.com>
2017-06-18 23:45:59 -0400

    added base validations to restaurant and reservations model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reservations
   belongs_to :owner
+
+  validates :name, :address, :capacity, :open_time, :close_time, presence: true
+
+  validates :capacity, numericality: { only_interger: true }
+
+  validates: :url, uniqueness: true
 end
