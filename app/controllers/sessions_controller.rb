0b2b3e1da8f4d1645bb158b9c8df816cbf352bcd
DocPrior <Sara Prior>
2017-06-20 14:34:35 -0400

    add authorizations to restaurants, users, and reservations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to restaurants_url, notice: "Logged In!"
     else
+      flash.now[:alert] = ["Login failed, email and/or password are incorrect!"]
       render "new"
     end
   end
